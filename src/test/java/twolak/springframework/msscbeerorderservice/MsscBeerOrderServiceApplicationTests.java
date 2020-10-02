f047fd1a41e754df7ffa2b100182fd6453802ac8
twolak88 <twolak88@gmail.com>
2020-10-02 23:15:50 +0200

    create initial project.

---
+++ src/test/java/twolak/springframework/msscbeerorderservice/MsscBeerOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package twolak.springframework.msscbeerorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
