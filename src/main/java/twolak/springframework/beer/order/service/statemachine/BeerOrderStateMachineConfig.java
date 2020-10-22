4fb7d72a687def14852024ba58437221b66be94b
twolak88 <twolak88@gmail.com>
2020-10-22 19:47:30 +0200

    Configure State Validation Transitions Closes #14

--- src/main/java/twolak/springframework/beer/order/service/statemachine/BeerOrderStateMachineConfig.java
+++ src/main/java/twolak/springframework/beer/order/service/statemachine/BeerOrderStateMachineConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 import twolak.springframework.beer.order.service.domain.BeerOrderEventEnum;
 import twolak.springframework.beer.order.service.domain.BeerOrderStatusEnum;
 
@@ -28,6 +29,17 @@ public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<B
                 .end(BeerOrderStatusEnum.VALIDATION_EXCEPTION)
                 .end(BeerOrderStatusEnum.ALLOCATION_EXCEPTION);
     }
-    
-    
+
+    @Override
+    public void configure(StateMachineTransitionConfigurer<BeerOrderStatusEnum, BeerOrderEventEnum> transitions) throws Exception {
+        transitions.withExternal()
+                    .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.NEW)
+                    .event(BeerOrderEventEnum.VALIDATE_ORDER)
+                .and().withExternal()
+                    .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATED)
+                    .event(BeerOrderEventEnum.VALIDATION_PASSED)
+                .and().withExternal()
+                    .source(BeerOrderStatusEnum.NEW).target(BeerOrderStatusEnum.VALIDATION_EXCEPTION)
+                    .event(BeerOrderEventEnum.VALIDATION_FAILED);
+    }
 }
