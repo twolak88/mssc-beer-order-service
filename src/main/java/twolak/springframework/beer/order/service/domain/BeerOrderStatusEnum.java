3f6c1a01efa6386991985cca3c00e65b58153e0d
twolak88 <twolak88@gmail.com>
2020-10-22 22:16:53 +0200

    Refactor Order States and Events Closes #18

--- src/main/java/twolak/springframework/beer/order/service/domain/BeerOrderStatusEnum.java
+++ src/main/java/twolak/springframework/beer/order/service/domain/BeerOrderStatusEnum.java
@@ -5,14 +5,16 @@ package twolak.springframework.beer.order.service.domain;
  * @author twolak
  */
 public enum BeerOrderStatusEnum {
-    NEW, 
-    VALIDATED, 
-    VALIDATION_EXCEPTION, 
-    ALLOCATED, 
-    ALLOCATION_EXCEPTION, 
-    PENDING_INVENTORY, 
-    PICKED_UP, 
-    DELIVERED, 
-    DELIVERY_EXCEPTION, 
+    NEW,
+    VALIDATION_PENDING,
+    VALIDATED,
+    VALIDATION_EXCEPTION,
+    ALLOCATION_PENDING,
+    ALLOCATED,
+    ALLOCATION_EXCEPTION,
+    PENDING_INVENTORY,
+    PICKED_UP,
+    DELIVERED,
+    DELIVERY_EXCEPTION,
     CANCELED
 }
